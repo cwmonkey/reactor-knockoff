54b89225352057009017665d8855f21fd901e280
cwmonkey <wackoburns@hotmail.com>
2015-09-24 23:54:55 -0700

    Adding some vent/heat alter logic, vent image, tile resizing for debugging, right click deletion

--- js/app.js
+++ js/app.js
@@ -12,6 +12,8 @@ var current_heat = 0;
 var current_power = 0;
 
 // For iteration
+var i;
+var l;
 var ri;
 var pi;
 var ci;
@@ -80,6 +82,15 @@ var parts = {
 		base_power_multiplier: 1,
 		base_heat_multiplier: 4,
 		location: 'cells'
+	},
+	vent: {
+		id: 'vent',
+		title: 'Small Heat Vent',
+		type: 'vent',
+		base_cost: 50,
+		base_containment: 80,
+		base_vent: 8,
+		location: 'cooling'
 	}
 };
 
@@ -92,6 +103,7 @@ var Tile = function() {
 	this.heat = 0;
 	this.power = 0;
 	this.ticks = 0;
+	this.vents = [];
 
 	if ( debug ) {
 		this.$heat = document.createElement('SPAN');
@@ -119,6 +131,8 @@ var Part = function(part) {
 	this.heat_multiplier = part.base_heat_multiplier;
 	this.power_multiplier = part.base_power_multiplier;
 	this.ticks = part.base_ticks;
+	this.containment = part.base_containment;
+	this.vent = part.base_vent;
 };
 
 // Operations
@@ -126,6 +140,8 @@ var tiler;
 var tileu;
 var tilel;
 var tiled;
+var tile_vent;
+var heat_remove;
 var update_tiles = function() {
 	for ( ri = 0; ri < rows; ri++ ) {
 		row = tiles[ri];
@@ -137,39 +153,142 @@ var update_tiles = function() {
 		}
 	}
 
+	// Alter counts
 	for ( ri = 0; ri < rows; ri++ ) {
 		row = tiles[ri];
+		tileu = null;
+		tiler = null;
+		tiled = null;
+		tilel = null;
 
 		for ( ci = 0; ci < cols; ci++ ) {
 			tile = row[ci];
+			tile.vents.length = 0;
+
 			if ( tile.part ) {
+				if ( ci < cols - 1 ) {
+					tiler = row[ci + 1];
+					if ( tiler.part && tiler.part.type === 'vent' ) {
+						tile.vents.push(tiler);
+					}
+				}
+
+				// left
+				if ( ci > 0 ) {
+					tilel = row[ci - 1];
+					if ( tilel.part && tilel.part.type === 'vent' ) {
+						tile.vents.push(tilel);
+					}
+				}
+
+				// down
+				if ( ri < rows - 1 ) {
+					tiled = tiles[ri + 1][ci];
+					if ( tiled.part && tiled.part.type === 'vent' ) {
+						tile.vents.push(tiled);
+					}
+				}
+
+				// up
+				if ( ri > 0 ) {
+					tileu = tiles[ri - 1][ci];
+					if ( tileu.part && tileu.part.type === 'vent' ) {
+						tile.vents.push(tileu);
+					}
+				}
+			}
+		}
+	}
+
+	// heat and power generators
+	for ( ri = 0; ri < rows; ri++ ) {
+		row = tiles[ri];
+		tileu = null;
+		tiler = null;
+		tiled = null;
+		tilel = null;
+
+		for ( ci = 0; ci < cols; ci++ ) {
+			tile = row[ci];
+
+			if ( tile.part ) {
+				// right
+				if ( ci < cols - 1 ) {
+					tiler = row[ci + 1];
+				}
+
+				// left
+				if ( ci > 0 ) {
+					tilel = row[ci - 1];
+				}
+
+				// down
+				if ( ri < rows - 1 ) {
+					tiled = tiles[ri + 1][ci];
+				}
+
+				// up
+				if ( ri > 0 ) {
+					tileu = tiles[ri - 1][ci];
+				}
+
 				if ( tile.part.type === 'cell' ) {
 					tile.heat += tile.part.heat;
 					tile.power += tile.part.power;
 
 					// neighbors
-					if ( ci < cols - 1 ) {
-						tiler = row[ci + 1];
+					// right
+					if ( tiler && tiler.part && tiler.part.type === 'cell' ) {
 						tiler.heat += tile.part.heat * tile.part.heat_multiplier;
 						tiler.power += tile.part.power * tile.part.power_multiplier;
 					}
 
-					if ( ci > 0 ) {
-						tilel = row[ci - 1];
+					// left
+					if ( tilel && tilel.part && tilel.part.type === 'cell' ) {
 						tilel.heat += tile.part.heat * tile.part.heat_multiplier;
 						tilel.power += tile.part.power * tile.part.power_multiplier;
 					}
 
-					if ( ri < rows - 1 ) {
-						tiled = tiles[ri + 1][ci];
+					// down
+					if ( tiled && tiled.part && tiled.part.type === 'cell' ) {
 						tiled.heat += tile.part.heat * tile.part.heat_multiplier;
 						tiled.power += tile.part.power * tile.part.power_multiplier;
 					}
 
-					if ( ri > 0 ) {
-						tileu = tiles[ri - 1][ci];
+					// up
+					if ( tileu && tileu.part && tileu.part.type === 'cell' ) {
 						tileu.heat += tile.part.heat * tile.part.heat_multiplier;
 						tileu.power += tile.part.power * tile.part.power_multiplier;
+					}
+				}
+
+			}
+		}
+	}
+
+	// alters
+	for ( ri = 0; ri < rows; ri++ ) {
+		row = tiles[ri];
+		tileu = null;
+		tiler = null;
+		tiled = null;
+		tilel = null;
+
+		for ( ci = 0; ci < cols; ci++ ) {
+			tile = row[ci];
+
+			if ( tile.part ) {
+				if ( tile.part.type === 'cell' ) {
+					l = tile.vents.length;
+
+					if ( l ) {
+						heat_remove = Math.ceil(tile.heat / l);
+
+						for ( i = 0; i < l; i++ ) {
+							tile_vent = tile.vents[i];
+							tile.heat -= heat_remove;
+							tile_vent.heat += heat_remove - tile_vent.part.vent;
+						}
 					}
 				}
 			}
@@ -193,6 +312,7 @@ var update_tiles = function() {
 var $reactor = document.getElementById('reactor');
 var $parts = document.getElementById('parts');
 var $cells = document.getElementById('cells');
+var $cooling = document.getElementById('cooling');
 var $current_heat = document.getElementById('current_heat');
 var $current_power = document.getElementById('current_power');
 
@@ -213,13 +333,17 @@ for ( ri = 0; ri < rows; ri++ ) {
 	tiles.push(row);
 }
 
-// create parts
+  /////////////////////////////
+ // Create Parts
+/////////////////////////////
 var part;
 for ( pi in parts ) {
 	part = new Part(parts[pi]);
 
 	if ( part.part.location === 'cells' ) {
 		$cells.appendChild(part.$el);
+	} else if ( part.part.location === 'cooling' ) {
+		$cooling.appendChild(part.$el);
 	}
 }
 
@@ -247,12 +371,18 @@ var tile_test = /^tile/;
 var part_replace = /[\b\s]part_[a-z0-9]+\b/;
 var spent_replace = /[\b\s]spent\b/;
 var tile_mousedown = false;
+var tile_mousedown_right = false;
 
 var apply_to_tile = function(e) {
 	if ( e.target.className.match(tile_test) ) {
 		tile = e.target.tile;
 
-		if ( clicked_part && ( tile.part !== clicked_part || tile.ticks === 0 ) ) {
+		if ( tile_mousedown_right ) {
+			tile.part = null;
+			tile.ticks = 0;
+			e.target.className = e.target.className.replace(part_replace, '').replace(spent_replace, '');
+			update_tiles();
+		} else if ( clicked_part && ( tile.part !== clicked_part || tile.ticks === 0 ) ) {
 			tile.part = clicked_part;
 			tile.ticks = clicked_part.ticks;
 			e.target.className = e.target.className.replace(part_replace, '').replace(spent_replace, '') + ' ' + clicked_part.className;
@@ -265,10 +395,18 @@ var tile_mouseup_fn = function(e) {
 	tile_mousedown = false;
 };
 
+document.oncontextmenu = function(e) {
+	if ( tile_mousedown_right ) {
+		e.preventDefault();
+		tile_mousedown_right = false;
+	}
+};
+
 $reactor.onclick = apply_to_tile;
 
 $reactor.onmousedown = function(e) {
 	tile_mousedown = true;
+	tile_mousedown_right = e.which === 3;
 	e.preventDefault();
 	apply_to_tile(e);
 };
