3f7e51fbbce4ce06bcba7bb1a85e10d93a751115
cwmonkey <wackoburns@hotmail.com>
2015-10-06 00:23:34 -0700

    Fixing nafastium, adding part tooltips, fixing heat/power/cost issues, adding upgrade structure, comments, game speed upgrade, styling tweaks

--- js/app.js
+++ js/app.js
@@ -7,20 +7,25 @@
 
 var cm_names = ("K M B T Qa Qi Sx Sp Oc No Dc").split(" ");
 
+var pow;
+var fnum;
+// http://userscripts-mirror.org/scripts/review/293573
 var fmt = function(num) {
   /*var len = Math.floor(num).toString().length - 1;
   var pow = Math.floor(len/3) * 3;
   var manlen = 2 - len % 3;
   return (Math.floor(num / Math.pow(10, pow - manlen)) / Math.pow(10, manlen)) + (pow == 0 ? "" : cm_names[(pow / 3) - 1]);*/
-  var pow = Math.floor((Math.floor(num).toString().length - 1)/3) * 3;
-  return (Math.floor(num / Math.pow(10, pow - 3)) / Math.pow(10, 3)) + (pow == 0 ? "" : cm_names[(pow / 3) - 1]);
+  pow = Math.floor((Math.floor(num).toString().length - 1)/3) * 3;
+  fnum = (Math.floor(num / Math.pow(10, pow - 3)) / Math.pow(10, 3)) + (pow == 0 ? "" : cm_names[(pow / 3) - 1]);
+
+  return fnum;
 };
 
 // settings
 var cols = 19;
 var rows = 16;
-var debug = false;
-var loop_wait = 1000;
+var debug = true;
+var base_loop_wait = 1000;
 var base_power_multiplier = 1;
 var base_heat_multiplier = 4;
 
@@ -30,6 +35,9 @@ var current_power = 0;
 var current_money = 0;
 var max_heat = 1000;
 var max_power = 100;
+var loop_wait = base_loop_wait;
+var power_multiplier = base_power_multiplier;
+var heat_multiplier = base_heat_multiplier;
 
 // For iteration
 var i;
@@ -39,6 +47,7 @@ var pi;
 var ci;
 var row;
 var tile;
+var upgrade;
 
 // Other vars
 var tiles = [];
@@ -52,7 +61,12 @@ var parts = {
 		base_cost: 10,
 		base_ticks: 15,
 		base_power: 1,
-		base_heat: 1
+		base_heat: 1,
+		enriched_upgrade_cost: 100,
+		enriched_upgrade_multiplier: 10,
+		potent_upgrade_cost: 500,
+		potent_upgrade_multiplier: 10,
+		perpetual_upgrade_cost: 10000
 	},
 	uranium2: {
 		id: 'uranium2',
@@ -126,10 +140,10 @@ var parts = {
 		levels: 3,
 		title: 'Nefastium Cell',
 		category: 'cell',
-		base_cost: 3922000000000,
-		base_ticks: 21600,
-		base_power: 226966000,
-		base_heat: 226966000
+		base_cost: 3586000000000000,
+		base_ticks: 86400,
+		base_power: 51871000000,
+		base_heat: 51871000000
 	},
 	vent: {
 		id: 'vent',
@@ -191,6 +205,38 @@ var Part = function(part) {
 	this.vent = part.base_vent;
 	this.cost = part.base_cost;
 	this.affordable = true;
+
+	var $image = document.createElement('DIV');
+	$image.className = 'image';
+	$image.innerHTML = 'Click to Select';
+
+	//this.$levels = document.createElement('SPAN');
+	//this.$levels.className = 'levels';
+
+	var $description = document.createElement('DIV');
+	$description.className = 'description info';
+
+	var $headline = document.createElement('DIV');
+	$headline.className = 'headline';
+	$headline.innerHTML = part.title;
+
+	//var $p = document.createElement('P');
+	//$p.className = 'text';
+	//$p.id = upgrade.id + 'text';
+	//$p.innerHTML = upgrade.description;*/
+
+	this.$cost = document.createElement('P');
+	this.$cost.className = 'cost';
+	this.$cost.innerHTML = fmt(this.cost);
+
+	//$image.appendChild(this.$levels);
+
+	$description.appendChild($headline);
+	//$description.appendChild($p);
+	$description.appendChild(this.$cost);
+
+	this.$el.appendChild($image);
+	this.$el.appendChild($description);
 };
 
 // Operations
@@ -297,29 +343,28 @@ var update_tiles = function() {
 					// neighbors
 					// right
 					if ( tiler && tiler.part && tiler.part.category === 'cell' && tiler.activated && tiler.ticks ) {
-						tiler.heat += tile.part.heat * tile.part.heat_multiplier;
-						tiler.power += tile.part.power * tile.part.power_multiplier;
+						tiler.heat += tile.part.heat * heat_multiplier;
+						tiler.power += tile.part.power * power_multiplier;
 					}
 
 					// left
 					if ( tilel && tilel.part && tilel.part.category === 'cell' && tilel.activated && tilel.ticks ) {
-						tilel.heat += tile.part.heat * tile.part.heat_multiplier;
-						tilel.power += tile.part.power * tile.part.power_multiplier;
+						tilel.heat += tile.part.heat * heat_multiplier;
+						tilel.power += tile.part.power * power_multiplier;
 					}
 
 					// down
 					if ( tiled && tiled.part && tiled.part.category === 'cell' && tiled.activated && tiled.ticks ) {
-						tiled.heat += tile.part.heat * tile.part.heat_multiplier;
-						tiled.power += tile.part.power * tile.part.power_multiplier;
+						tiled.heat += tile.part.heat * heat_multiplier;
+						tiled.power += tile.part.power * power_multiplier;
 					}
 
 					// up
 					if ( tileu && tileu.part && tileu.part.category === 'cell' && tileu.activated && tileu.ticks ) {
-						tileu.heat += tile.part.heat * tile.part.heat_multiplier;
-						tileu.power += tile.part.power * tile.part.power_multiplier;
+						tileu.heat += tile.part.heat * heat_multiplier;
+						tileu.power += tile.part.power * power_multiplier;
 					}
 				}
-
 			}
 		}
 	}
@@ -380,6 +425,7 @@ var $max_heat = document.getElementById('max_heat');
 var $max_power = document.getElementById('max_power');
 var $save = document.getElementById('save');
 var $main = document.getElementById('main');
+var $upgrades = document.getElementById('upgrades');
 
 if ( debug ) {
 	$main.className += ' debug';
@@ -408,6 +454,7 @@ for ( ri = 0; ri < rows; ri++ ) {
   /////////////////////////////
  // Create Parts
 /////////////////////////////
+
 var part_obj;
 var part_settings;
 var part;
@@ -419,7 +466,6 @@ var cell_power_multipliers = [1, 4, 12];
 var cell_heat_multipliers = [1, 8, 36];
 
 var create_part = function(part, level) {
-
 	if ( level ) {
 		part = JSON.parse(JSON.stringify(part));
 		part.level = level;
@@ -427,7 +473,10 @@ var create_part = function(part, level) {
 		if ( part.category === 'cell' ) {
 			part.id = part.type + level;
 			part.title = cell_prefixes[level -1] + part.title;
-			part.base_cost = part.base_cost * Math.pow(22, level -1) / 10;
+			part.base_cost = part.base_cost
+			if ( level > 1 ) {
+				part.base_cost *= Math.pow(22, level - 1) / 10;
+			}
 			part.base_power = part.base_power * cell_power_multipliers[level];
 			part.base_heat = part.base_heat * cell_heat_multipliers[level];
 		} else {
@@ -463,17 +512,274 @@ for ( pi in parts ) {
 }
 
   /////////////////////////////
- // Events
+ // Upgrades
 /////////////////////////////
-// Save game
+
+var upgrades = [
+	{
+		id: 'chronometer',
+		type: 'other',
+		title: 'Improved Chronometers',
+		description: '+1 tick per second per level of upgrade.',
+		cost: 10000,
+		multiplier: 100,
+		onclick: function(upgrade) {
+			loop_wait = base_loop_wait / ( upgrade.level + 1 );
+		}
+	},
+	{
+		id: 'forceful_fusion',
+		type: 'other',
+		title: 'Forceful Fusion',
+		description: 'Cells produce 1% more power at 1k heat, 2% power at 2m heat etc. per level of upgrade.',
+		cost: 10000,
+		multiplier: 100,
+		onclick: function(upgrade) {
+
+		}
+	},
+	{
+		id: 'improved_piping',
+		type: 'other',
+		title: 'Improved Piping',
+		description: 'Venting manually is 10x as effective per level of upgrade.',
+		cost: 100,
+		multiplier: 20,
+		onclick: function(upgrade) {
+			
+		}
+	},
+	{
+		id: 'improved_alloys',
+		type: 'other',
+		title: 'Improved Alloys',
+		description: 'Plating holds 100% more heat per level of upgrade.',
+		cost: 5000,
+		multiplier: 5,
+		onclick: function(upgrade) {
+			
+		}
+	},
+	{
+		id: 'improved_power_lines',
+		type: 'other',
+		title: 'Improved Power Lines',
+		description: 'Sells 1% of your power each tick per level of upgrade.',
+		cost: 100,
+		multiplier: 10,
+		onclick: function(upgrade) {
+			
+		}
+	},
+	{
+		id: 'improved_wiring',
+		type: 'other',
+		title: 'Improved Wiring',
+		description: 'Capacitors hld +100% power and heat per level of upgrade.',
+		cost: 5000,
+		multiplier: 5,
+		onclick: function(upgrade) {
+			
+		}
+	},
+
+	{
+		id: 'improved_coolant_cells',
+		type: 'other',
+		title: 'Improved Coolant Cells',
+		description: 'Coolant cells hold 100% more heat per level of upgrade.',
+		cost: 5000,
+		multiplier: 100,
+		onclick: function(upgrade) {
+			
+		}
+	},
+
+	{
+		id: 'improved_reflectors',
+		type: 'other',
+		title: 'Improved Reflectors',
+		description: 'Reflectors last 100% longer per level of upgrade.',
+		cost: 5000,
+		multiplier: 100,
+		onclick: function(upgrade) {
+			
+		}
+	},
+
+	// Exchangers
+	{
+		id: 'improved_heat_exchangers',
+		type: 'exchangers',
+		title: 'Improved Heat Exchangers',
+		description: 'Heat Exchangers hold and exchange 100% more heat per level of upgrade',
+		cost: 600,
+		multiplier: 100,
+		onclick: function(upgrade) {
+			
+		}
+	},
+	{
+		id: 'reinforced_heat_exchangers',
+		type: 'exchangers',
+		title: 'Reinforced Heat Exchangers',
+		description: 'Each plating increases the effectiveness of exchangers by 1% per level of upgrade per level of plating.',
+		cost: 1000,
+		multiplier: 100,
+		onclick: function(upgrade) {
+			
+		}
+	},
+	{
+		id: 'active_exchangers',
+		type: 'exchangers',
+		title: 'Active Exchangers',
+		description: 'Each capacitor increases the effectiveness of exchangers by 1% per level of upgrade per level of capacitor.',
+		cost: 1000,
+		multiplier: 100,
+		onclick: function(upgrade) {
+			
+		}
+	},
+
+	// Vents
+	{
+		id: 'improved_heat_vents',
+		type: 'vents',
+		title: 'Improved Heat Vents',
+		description: 'Vents hold and vent 100% more heat per level of upgrade.',
+		cost: 250,
+		multiplier: 100,
+		onclick: function(upgrade) {
+			
+		}
+	},
+	{
+		id: 'improved_heatsinks',
+		type: 'vents',
+		title: 'Improved Heatsinks',
+		description: 'Each plating increases the effectiveness of vents by 1% per level of upgrade per level of plating.',
+		cost: 1000,
+		multiplier: 100,
+		onclick: function(upgrade) {
+			
+		}
+	},
+	{
+		id: 'active_venting',
+		type: 'vents',
+		title: 'Active Venting',
+		description: 'Each capacitor increases the effectiveness of vents by 1% per level of upgrade per level of capacitor.',
+		cost: 1000,
+		multiplier: 100,
+		onclick: function(upgrade) {
+			
+		}
+	}
+];
+
+var Upgrade = function(upgrade) {
+	var me = this;
+	this.upgrade = upgrade;
+	this.level = null;
+	this.cost = null;
+	this.$el = document.createElement('BUTTON');
+	this.$el.className = 'upgrade';
+	this.$el.id = upgrade.id;
+
+	var $image = document.createElement('DIV');
+	$image.className = 'image';
+	$image.innerHTML = 'Click to Upgrade';
+
+	this.$levels = document.createElement('SPAN');
+	this.$levels.className = 'levels';
+
+	var $description = document.createElement('DIV');
+	$description.className = 'description info';
+
+	var $headline = document.createElement('DIV');
+	$headline.className = 'headline';
+	$headline.id = upgrade.id + 'headline';
+	$headline.innerHTML = upgrade.title;
+
+	var $p = document.createElement('P');
+	$p.className = 'text';
+	$p.id = upgrade.id + 'text';
+	$p.innerHTML = upgrade.description;
+
+	this.$cost = document.createElement('P');
+	this.$cost.className = 'cost';
+
+	$image.appendChild(this.$levels);
+
+	$description.appendChild($headline);
+	$description.appendChild($p);
+	$description.appendChild(this.$cost);
+
+	this.$el.appendChild($image);
+	this.$el.appendChild($description);
+
+	this.setLevel(0);
+};
+
+Upgrade.prototype.setLevel = function(level) {
+	this.level = level;
+	this.$levels.innerHTML = level;
+	this.cost = this.upgrade.cost * Math.pow(this.upgrade.multiplier, this.level);
+	this.$cost.innerHTML = fmt(this.cost);
+	this.upgrade.onclick(this);
+}
+
+var upgrade_locations = {
+	ticks: document.getElementById('cell_tick_upgrades'),
+	powers: document.getElementById('cell_power_upgrades'),
+	perpetuals: document.getElementById('cell_perpetual_upgrades'),
+	other: document.getElementById('other_upgrades'),
+	vents: document.getElementById('vent_upgrades'),
+	exchangers: document.getElementById('exchanger_upgrades')
+};
+
+var upgrade_objects = {};
+var upgrade_objects_array = [];
+var create_upgrade = function(u) {
+	var upgrade = new Upgrade(u);
+	upgrade.$el.upgrade = upgrade;
+	upgrade_locations[u.type].appendChild(upgrade.$el);
+	upgrade_objects_array.push(upgrade);
+	upgrade_objects[upgrade.upgrade.id] = upgrade;
+};
+
+for ( var i = 0, l = upgrades.length; i < l; i++ ) {
+	create_upgrade(upgrades[i]);
+}
+
+// Upgrade event
+var upgrade_test = /^upgrade/;
+$upgrades.onclick = function(e) {
+	if ( e.target.className.match(upgrade) ) {
+		var upgrade = e.target.upgrade;
+
+		if ( upgrade && current_money >= upgrade.cost ) {
+			current_money -= upgrade.cost;
+			$money.innerHTML = fmt(current_money);
+			upgrade.setLevel(upgrade.level + 1);
+		}
+	}
+};
+
+  /////////////////////////////
+ // Save game
+/////////////////////////////
+
 var srows;
 var spart;
 var sstring;
 var squeue;
+var supgrades;
 var save = function() {
 	srows = [];
-	squeue = [];
 
+	// Tiles
 	for ( ri = 0; ri < rows; ri++ ) {
 		row = tiles[ri];
 		srow = [];
@@ -496,14 +802,30 @@ var save = function() {
 		srows.push(srow);
 	}
 
+	// Tile queue
+	squeue = [];
 	for ( i = 0, l = tile_queue.length; i < l; i++ ) {
 		tile = tile_queue[i];
-		squeue.push({row: tile.row, col: tile.col});
+		squeue.push({
+			row: tile.row,
+			col: tile.col
+		});
+	}
+
+	// Upgrades
+	supgrades = [];
+	for ( i = 0, l = upgrade_objects_array.length; i < l; i++ ) {
+		upgrade = upgrade_objects_array[i];
+		supgrades.push({
+			id: upgrade.upgrade.id,
+			level: upgrade.level
+		});
 	}
 
 	window.localStorage.setItem('rks', window.btoa(JSON.stringify({
 		tiles: srows,
 		tile_queue: squeue,
+		upgrades: supgrades,
 		current_power: current_power,
 		current_money: current_money,
 		current_heat: current_heat
@@ -590,7 +912,7 @@ var mouse_apply_to_tile = function(e) {
 			&& (
 				!tile.part
 				|| (tile.part === clicked_part && tile.ticks === 0)
-				|| (tile.part && tile.part.type === clicked_part.type && tile.part.level < clicked_part.level && current_money > clicked_part.cost )
+				|| (tile.part && tile.part.type === clicked_part.type && tile.part.part.level < clicked_part.part.level && current_money >= clicked_part.cost )
 			)
 		) {
 			if ( current_money < clicked_part.cost ) {
@@ -613,16 +935,20 @@ var mouse_apply_to_tile = function(e) {
   /////////////////////////////
  // Load
 /////////////////////////////
+
 var stile;
+var supgrade;
 var rks = window.localStorage.getItem('rks');
 var srow;
 if ( rks ) {
 	rks = JSON.parse(window.atob(rks));
 
+	// Current values
 	$current_heat.innerHTML = fmt(current_heat = rks.current_heat || current_heat);
 	$current_power.innerHTML = fmt(current_power = rks.current_power || current_power);
 	$money.innerHTML = fmt(current_money = rks.current_money || current_money);
 
+	// Tiles
 	for ( ri = 0; ri < rows; ri++ ) {
 		row = tiles[ri];
 		srow = rks.tiles[ri];
@@ -642,13 +968,24 @@ if ( rks ) {
 		}
 	}
 
+	// Tile queue
 	for ( i = 0, l = rks.tile_queue.length; i < l; i++ ) {
 		stile = rks.tile_queue[i];
 		tile_queue.push(tiles[stile.row][stile.col]);
 	}
 
 	update_tiles();
+
+	// Upgrades
+	for ( i = 0, l = rks.upgrades.length; i < l; i++ ) {
+		supgrade = rks.upgrades[i];
+		upgrade_objects[supgrade.id].setLevel(supgrade.level);
+	}
 }
+
+  /////////////////////////////
+ // Tile clicks
+/////////////////////////////
 
 var tile_mouseup_fn = function(e) {
 	tile_mousedown = false;
@@ -702,10 +1039,9 @@ $scrounge.onclick = function() {
   /////////////////////////////
  // Game Loop
 /////////////////////////////
-var loop_interval;
+var loop_timeout;
 
-loop_interval = setInterval(function() {
-
+var game_loop = function() {
 	for ( ri = 0; ri < rows; ri++ ) {
 		row = tiles[ri];
 
@@ -744,7 +1080,8 @@ loop_interval = setInterval(function() {
 	$current_heat.innerHTML = fmt(current_heat);
 	$current_power.innerHTML = fmt(current_power);
 
-}, loop_wait);
+	loop_timeout = setTimeout(game_loop, loop_wait);
+};
 
 // affordability loop
 var unaffordable_replace = /[\s\b]unaffordable\b/;
@@ -762,6 +1099,7 @@ var check_affordability = function() {
 };
 
 check_affordability();
+game_loop();
 
 setInterval(check_affordability, 1000);
 
